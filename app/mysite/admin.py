106f581f26524625268af06df22394505f5c1399
palladog <milobengan@gmail.com>
2018-04-19 17:49:38 +0200

    Skapade admin.py

---
+++ app/mysite/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from myproject.myapp.models import Author
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+
--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -44,9 +44,9 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
