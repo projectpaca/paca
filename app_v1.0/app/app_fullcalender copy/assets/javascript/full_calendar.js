744b462ea0e1454a265cbc223b48dc8a96cea611
Hanna <fellwinghanna@gmail.com>
2018-04-23 11:20:33 +0200

    Full_Calendar 1.0
    Uppdaterad version fullcalendar

---
+++ app_v1.0/app/app_fullcalender copy/assets/javascript/full_calendar.js
@@ -0,0 +1,150 @@
+//JavaScript kod från ramverket FullCalendar
+// För projektet PACA, skivet av Hanna 
+$(function() {
+
+  // funktionen startar
+
+    $('#calendar').fullCalendar({
+        timeZone: 'local',
+        defaultView: 'month',
+        fixedWeekCount: false,
+        firstDay: 0, 
+        // sätter första dagen i månaden & veckan till måndag
+        weekNumbers: true,
+        weekNumberTitle: 'Vecka',
+        // skapar en kolumn för vecka samt ändrar kolumnens namn till 'Vecka'
+
+        lazyFetching: false,
+        /* Varje gång anävndaren klickar på någonting i kalendern kommer infomrmation hämtas
+        användbart ifall det är många användare samtidigt. Motsvarigheten 'true' gör istället
+        att all information hämtas in i för varje månad & går därför snabbare */
+        handleWindowResize: true,
+        // anpassar kalendern till fönstret
+
+        header: {
+            left: 'title',
+            center: 'prev, next',
+            right: 'month, agendaWeek, agendaMonth'
+        },
+        // fixerar headern så månaden står till vänster, pilarna för nästa & föregående i mitten och månad/vecka till höger
+        buttonText: {
+            today: 'idag',
+            month: 'månad',
+            week: 'vecka',
+            day: 'dag'
+        },
+        eventLimit: true,
+        slotEventOverlap: true,
+        minTime: '05:00:00',
+        maxTime: '22:00:00',
+        // gör det möjligt för event att lägga sig över varandra
+        events: [
+            {
+                title: 'TEST',
+                start: '2018-04-20',
+                end: '2018-04-20',
+            }
+        ], //ett hårdkodat event för test **
+        displayEventTime: true,
+        // senare när eventen har inskrivna start- och sluttid så kommer dessa visas
+        monthNames: [
+            'Januari', 'Februari', 'Mars',
+            'April', 'Maj', 'Juni', 'Juli',
+            'Augusti', 'September','Oktober',
+            'November', 'December'
+        ],
+        // översätter namnen på månaderna till svenska
+        dayNamesShort: [
+            'Mån', 'Tis', 'Ons', 'Tors',
+            'Fre', 'Lör', 'Sön'
+        ],
+        // översätter namnen på dagarna till svenska
+
+        /*
+        columnHeaderFormat: {
+            week: 'ddd M/D',
+            day: 'dddd M/D'
+        },
+        - En funktion jag ej fick att fungera, men tänkt att det ska visa första tre bokstäverna på dagen samt datumet i veckovy och i dagvy visa hela namnet på dagen och datum
+        */
+
+        titleFormat: 'MMMM YYYY',
+        // för att visa hela namnet på månaden samt året till vänster uppe i header
+        timeFormat: 'h(:mm)',
+        // visar tiden på pass. Som hela timmar enligt 24h klockan och minuter, ex 06.30
+        weekends: true,
+        editable: true,
+        droppable: true,
+        selectable: true,
+        selectHelper: true,
+        
+        select: function(start, end) {
+            var duration = (end - start) / 1000;
+            if(duration === 1800) {
+            // sätter default varaktighet till en timme
+                end = start.add(30, 'mins');
+                return $('#calendar').fullCalendar('select', start, end);
+            }
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title && title.trim()) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+
+                $('#calendar').fullCalendar('renderEvent', {
+                    start: start,
+                    end: end,
+                    title: title
+                },  'stick',
+                    true);
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        
+        eventRender: function(event, element) {
+            var start = moment(event.start).fromNow();
+            element.attr('title', start);
+        },
+        loading: function() {
+      
+        }
+        
+        /* räknar ut hur länge ett event är från start- och sluttid. Kontrollerar att eventet är 
+        tillräckligt lång och att det finns en instriven titel. Titeln på eventet blir den angivna
+        titeln, startiden blir den dag/tid användaren valt och sluttid där användaren släppt/inmatat
+        tid eller datum */
+        
+
+      /*  select: function(start, end, jsEvent, view) {
+            var title = prompt("Skriv in namnet på detta event", "Nytt event");
+ 
+            if (title !== null) {
+            // Create event
+                var event = {
+                        title: title.trim() !== "" ? title : "Nytt event",
+                        start: start,
+                        end: end
+                    };
+                $calendar.fullCalendar("renderEvent", event, true);
+            }
+        } */
+    })
+
+});
+
+$('#calendar').fullCalendar('next');
+// för att komma till nästa vecka eller månad
+
+$('#calendar').fullCalendar({
+    navLinks: true,
+    navLinkDayClick: function(date, jsEvent) {
+        console.log('day', date.format()); // date is a moment
+        console.log('coords', jsEvent.pageX, jsEvent.pageY);
+    } // länkar siffran (dagens datum) i kalendern till specifika sidan för den dagen
+
+});
+
+                   
