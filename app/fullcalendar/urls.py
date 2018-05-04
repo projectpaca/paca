c2e364b29995e172620b0256ed6e68623aaec356
victordahl <v.d@live.se>
2018-05-04 21:00:21 +0200

    Fullcalendar hämtar från SQLite!

--- app/fullcalendar/urls.py
+++ app/fullcalendar/urls.py
@@ -7,19 +7,14 @@ from .views import NewShift
 # Detta är namespacing!
 
 urlpatterns = [
-    # Allt här ligger efter /calendar
     path( r'', views.fullcalendar, name='fullcalendar'),
 
-    #Hämtar alla events i JSON format
+    # Hämtar alla events i JSON format
     path(r'events', views.events),
 
     # Sparar ett nytt event till databasen
     path( r'new', views.new),
-        #NewShift.as_view()),
 
-    #path( r'new$', NewShift.as_view()),
-    #path( r'new', views.new_shift, name='new_shift'),
-        # Ska new$ peka på fullcalendar igen
-        # eller gör det så att sidan laddas om?
-    path(r'<int:shift_id>', views.detail, name='detail'),
+    # Tänkt att visa info om ett pass
+    # path(r'<int:shift_id>', views.detail, name='detail'),
 ]
