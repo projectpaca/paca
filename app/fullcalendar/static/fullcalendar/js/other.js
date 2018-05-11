fc07c708c68250c3c34d953b2e2740d3b9ec1e89
Hanna <fellwinghanna@gmail.com>
2018-05-11 16:13:49 +0200

    stampar med fullcalendar så börjat lite på annan js, markera aktiv länk gör detta

---
+++ app/fullcalendar/static/fullcalendar/js/other.js
@@ -0,0 +1,18 @@
+function markLinks() {
+
+    var linkDiv = document.getElementById("navID"),
+// navID är id på diven som håller länkarna
+
+        links = linkDiv.getElementsByClassName("link");
+// link är en klass, som varje länk har i sin a-tag
+
+    for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+// när användaren klickar på en länk så ändras statusen på den länken
+// till "active". Endast en länk i taget kan vara aktiv.  
+}
