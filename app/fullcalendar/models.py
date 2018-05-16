8b2a274b05a26547d07dd465c93644a229515f0b
victordahl <v.d@live.se>
2018-05-16 18:04:26 +0200

    Påbörjat accounts, parallelljobbat på fullcal

--- app/fullcalendar/models.py
+++ app/fullcalendar/models.py
@@ -3,13 +3,17 @@ from django.db import models
 from django.db.models import DateTimeField
 from django.utils import timezone
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 class Event(models.Model):
     """ Grundläggande DB schema för calender """
     title = models.CharField(max_length=100)
     start = models.DateTimeField('start')
     end = models.DateTimeField('end')
-    req_usr = models.int()
+    req_usr = models.PositiveIntegerField()
+    # dep = GenericForeignKey?
 
     def __str__(self):
         return self.title
@@ -17,7 +21,7 @@ class Event(models.Model):
 class BookedUser(models.Model):
     """ Visar vilka anställda som är bokade på vilka pass """
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = GenericForeignKey('content_type', 'CustomUser')
     # Osäker på om user "CustomUser" är korrekt, eller om den
     # behöver importeras åt något håll först?
 
