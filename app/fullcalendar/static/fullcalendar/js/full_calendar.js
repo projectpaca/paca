e253645c37d4e9758bfd1785dca020f1212c3688
Hanna <fellwinghanna@gmail.com>
2018-05-17 15:15:12 +0200

    färg på events, kan inte längre lägga till event då endast admin ska göra det från adminsidan

--- app/fullcalendar/static/fullcalendar/js/full_calendar.js
+++ app/fullcalendar/static/fullcalendar/js/full_calendar.js
@@ -66,18 +66,57 @@ $(function () {
         
         //funktionalitet i kalendern
         weekends: true,
-        editable: true,
-        droppable: true,
+        //editable: true,
         eventDurationEditable: true,
         eventStartEditable: true,
-        eventSources: [
-            {
-                url: 'events'
-            }],
-        selectable: true,
-        selectHelper: true,
+        eventSources: [{
+            url: 'events'
+        }],
+        eventMouseover: function (data, event, view) {
 
-        select: function (start, end, jsEvent, view) {
+            tooltip = '<div class="tooltiptopicevent" style="width:auto;height:auto;background:#FFE4B5;position:absolute;z-index:10001;padding:10px 10px 10px 10px ;  line-height: 200%;">' + 'Titel: ' + data.title + '</br>' + 'Börjar: ' + data.start.format('YYYY-MM-DD hh:mm') + '</br>' + 'Slutar: ' + data.end.format('YYYY-MM-DD hh:mm') + '</div>';
+
+
+            $("body").append(tooltip);
+            $(this).mouseover(function (e) {
+                $(this).css('z-index', 10000);
+                $('.tooltiptopicevent').fadeIn('500');
+                $('.tooltiptopicevent').fadeTo('10', 1.9);
+            }).mousemove(function (e) {
+                $('.tooltiptopicevent').css('top', e.pageY + 10);
+                $('.tooltiptopicevent').css('left', e.pageX + 20);
+            });
+
+
+        },
+        eventMouseout: function (data, event, view) {
+            $(this).css('z-index', 8);
+
+            $('.tooltiptopicevent').remove();
+
+        },
+            
+        eventRender: function (event, element, view) {
+            
+            if (event.title === 'Kassa') {
+                element.css('eventBackgroundColor', '#B2FF66');
+                
+            } else if (event.title === 'Lager') {
+                element.css('eventBackgroundColor', '#FF3333');
+            
+            } else if (event.title === 'Chark') {
+                element.css('eventBackgroundColor', '#66B2FF'); 
+            }
+
+        },
+        eventColor: '#FFCCCC'  
+    });
+});
+
+$('#calendar').fullCalendar('next');
+
+
+       /* select: function (start, end, jsEvent, view) {
             var duration = (end - start) / 1000,
                 title = prompt('Skriv in titel på passet:', "Nytt pass"),
                 req_usr = "1",
@@ -89,11 +128,11 @@ $(function () {
                     req_usr: req_usr,
                     dept: dept
                 };
-            /*
+                
             if (duration === 1800) {
                 end = start.add(30, 'mins');
                 return $('#calendar').fullCalendar('select', start, end);
-            } */
+            } 
 
             if (title !== null) {
                 $("#calendar").fullCalendar('renderEvent', event, 'stick', true);
@@ -104,59 +143,58 @@ $(function () {
             // var csrftoken = Cookies.get('csrftoken');
             var csrftoken = getCookie('csrftoken');
             console.log(csrftoken);
-
             
-            // Lägg till event
-            $.ajax({
-                type: "POST",
-                url: 'new',
-                //url: "{{=URL('new')}}?event=" + JSON.stringify(event),
-                headers: {
-                    'X-CSRFToken': csrftoken
-                },
-                //+ JSON.stringify(event),
-                data: event, // (titel, start, end, req_usr, dept)
-                dataType: "json",
-                success: function () {
-                    alert("YEEEYYY!");
-                },
-                error: function () {
-                    alert("nix inte idag...");
-                }
+            $('#calendar').fullcalendar({
+                eventClick: function (event, jsEvent, view) {
+                    if (alert("Vill du ta bort detta pass?")) {
+                        $('#calendar').fullCalendar('removeEvents', event);
+                    } else {
+                        pass;
+                    }
+                    
+                    $('#calendar').fullCalendar('updateEvent', event);
+
+                }, 
                 
-            }).done(function (data) {
-                $("#calendar").fullCalendar("renderEvent", data);
-            });
+            }); */
+
+            
+    /*        // Lägg till event
+    $.ajax({
+        type: "POST",
+        url: 'new',
+        headers: {
+            'X-CSRFToken': csrftoken
+        },
+                //+ JSON.stringify(event),
+        data: event, // (titel, start, end, req_usr, dept)
+        dataType: "json",
+        success: function () {
+            alert("YEEEYYY!");
+        },
+        error: function () {
+            alert("nix inte idag...");
+        }
+                
+    }).done(function (data) {
+        $("#calendar").fullCalendar("renderEvent", data);
+        $("#myModal").modal("hide");
+
+    });
             
         
-        }, //select 
-        eventRender: function (event, element) {
-            var start = moment(event.start).fromNow();
-            element.attr('title', start);
-        }
+}); //select 
+
+*/
         
-    });// function fullcalendar
-});//function
+//    });// function fullcalendar
+// });//function
 
-
-$('#calendar').fullCalendar('next');
-
-/*$('#calendar').fullCalendar({
-    removeEvents:  pass
-}); */
-
-
-
+/*
 $('#calendar').fullCalendar({
-    DayClick: function (date, jsEvent) {
+    DayClick: function (date, jsEvent, view) {
         console.log('day', date.format()); // date is a moment
         console.log('coords', jsEvent.pageX, jsEvent.pageY);
     } // länkar siffran (dagens datum) i kalendern till specifika sidan för den dagen
-  /*  eventClick: function (event, jsEvent, view) {
-        if (confirm("Vill du ta bort detta pass?")) {
-           $('#calendar').fullCalendar('removeEvents', [idOrFilter], event);
-        } else {
-            pass
-        }
-    } */
-});
+ 
+});*/
