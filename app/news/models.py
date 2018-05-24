71f710e6f1398d651079166ce0d5682a5b2c2425
Lisa Wasslöv <lisawasslov@hotmail.com>
2018-05-24 13:35:59 +0200

    Nyheter fungerar
    Fixade även så att nyheter sorteras efter '-date' istället för 'date', eftersom det sistnämnda gjorde att den äldsta visades först.

--- app/news/models.py
+++ app/news/models.py
@@ -1,39 +1,29 @@
 from django.db import models
 from django.conf.urls import include, url
-from app import settings
-from userauth.models import CustomUser
+#from app import settings
 
+#class Post(models.Model):
+#    title = models.CharField(max_length=200)
+#    author = models.ForeignKey(
+#        'settings.AUTH_USER_MODEL',
+#        on_delete=models.CASCADE,
+#    )
+#    body = models.TextField()
+#
+#    def __str__(self):
+#        return self.title
 
+#####
 class News(models.Model):
-	author = models.ForeignKey(
-		CustomUser,
-		'Författare',
-		null=True,
-	)
-	title = models.CharField(
-		'Rubrik',
-		max_length=100,
-	)
-	#slug = models.SlugField()
-	content = models.TextField(
-		'Brödtext',
-	)
-	date = models.DateTimeField(
-		auto_now_add=True, 
-		null=True, 
-		blank=True
-	)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    #add in thumbnail
+    #add in author
 
-	list_display = ('title', 'author', 'date')
-	search_fields = ['title','author','date']
-	list_filter = ['author','date']
+    def __str__(self):
+        return self.title
 
-	class Meta:
-			verbose_name = 'nyhet'
-			verbose_name_plural = 'nyheter'
-	
-	def __str__(self):
-		return self.title
-
-	def snippet(self):
-		return self.body[:50] + '...'
+    def snippet(self):
+        return self.body[:50] + '...'
--- app/news/views.py
+++ app/news/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News
 
-
 def news(request):
-	''' '''
-	news = News.objects.all()
-	return render(request, 'news/news_list.html', {"news":news})
+    ''' '''
+    news = News.objects.all().order_by('-date')
+    return render(request, 'news_list.html', {'news':news})
 
-def news_detail(request, slug):
-	''' '''
-	news = News.objects.get(slug=slug)
-	return render(request, 'news/news_detail.html', {"news":news})
+def news_detail(request,slug):
+    #return HttpResponse(slug)
+    news = News.objects.get(slug=slug)
+    return render(request, 'news/news_detail.html', {'news':news})
